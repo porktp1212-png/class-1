8761a5a92154461eff0ba49332b8c3b3054a3821
porktp1212-png <porktp1212@gmail.com>
2026-09-23 21:30:17 +0700

    Create evaluate-submission.ts

---
+++ api/api/ai/evaluate-submission.ts
@@ -0,0 +1,10 @@
+import { handleApiRequest } from "../../server/apiRouter.ts";
+
+export default async function handler(req: any, res: any) {
+  const handled = await handleApiRequest(req, res);
+
+  if (!handled && !res.headersSent) {
+    res.statusCode = 404;
+    res.end("API route not found");
+  }
+}
